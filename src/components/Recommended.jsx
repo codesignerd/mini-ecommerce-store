7b77fa76a36d61767ccdfca68b98037376fe1b72
Blessing Abayomi <blessingabayomi0204@gmail.com>
2026-04-27 14:21:11 +0100

    implemented logic for tag filter

--- src/components/Recommended.jsx
+++ src/components/Recommended.jsx
@@ -1,11 +1,17 @@
-const Recommended = () => {
-  const tags = ['All', 'Nike', 'Adidas', 'Zara', 'H&M'];
+const Recommended = ({ handleFilterChange }) => {
+  const tags = ['All', 'Nike', 'Adidas', 'Zara', 'H&M', 'Puma'];
 
   return (
     <div className="flex gap-3 flex-wrap mb-4">
       {tags.map((tag) => (
         <button
           key={tag}
+          onClick={() =>
+            handleFilterChange(
+              'brand',
+              tag.toLocaleLowerCase() === 'all' ? '' : tag,
+            )
+          }
           className="px-4 py-2 rounded-md text-sm border-2 border-gray-200 hover:bg-orange-500 hover:text-white transition cursor-pointer"
         >
           {tag}
