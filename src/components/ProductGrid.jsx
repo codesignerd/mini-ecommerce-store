988245d8bf64200aa9b4642b7c5176179a611773
Blessing Abayomi <blessingabayomi0204@gmail.com>
2026-04-27 12:12:27 +0100

    imported products from data

--- src/components/ProductGrid.jsx
+++ src/components/ProductGrid.jsx
@@ -1,13 +1,10 @@
 import ProductCard from './ProductCard';
+import products from '../data';
 
 const ProductGrid = () => {
-  const dummy = [1, 2, 3, 4, 5, 6, 7, 8];
-
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-      {dummy.map((item) => (
-        <ProductCard key={item} />
-      ))}
+      <ProductCard products={products} />
     </div>
   );
 };
