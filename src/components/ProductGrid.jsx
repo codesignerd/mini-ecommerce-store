8bf619cbc51f257df1f014f826541019cc14ae29
Blessing Abayomi <blessingabayomi0204@gmail.com>
2026-04-27 14:26:27 +0100

    implemented conditional rendering for product search

--- src/components/ProductGrid.jsx
+++ src/components/ProductGrid.jsx
@@ -1,11 +1,20 @@
 import ProductCard from './ProductCard';
-import products from '../data';
 
-const ProductGrid = () => {
+const ProductGrid = ({ products }) => {
   return (
-    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-      <ProductCard products={products} />
-    </div>
+    <>
+      {products.length > 0 ? (
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          {products.map((product) => (
+            <ProductCard product={product} />
+          ))}
+        </div>
+      ) : (
+        <div className="flex justify-center py-6">
+          <p>Found {products.length} products that matches the search</p>
+        </div>
+      )}
+    </>
   );
 };
 
