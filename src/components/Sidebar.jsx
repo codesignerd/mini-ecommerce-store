d52fadb190b81d0a810a404796e5b1bb7fc7546e
Blessing Abayomi <blessingabayomi0204@gmail.com>
2026-04-27 09:49:46 +0100

    updated sidebar styling

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,10 @@
 const Sidebar = () => {
   return (
-    <div className="space-y-6">
+    <div className="flex flex-col gap-8 pt-10">
       {/* Category */}
       <div>
         <h3 className="font-semibold mb-2">Category</h3>
-        <div className="space-y-1 text-sm">
+        <div className="space-y-1 text-base">
           <p className="cursor-pointer hover:text-orange-500">T-Shirts</p>
           <p className="cursor-pointer hover:text-orange-500">Hoodies</p>
           <p className="cursor-pointer hover:text-orange-500">Jackets</p>
@@ -15,7 +15,7 @@ const Sidebar = () => {
       {/* Price */}
       <div>
         <h3 className="font-semibold mb-2">Price</h3>
-        <div className="space-y-1 text-sm">
+        <div className="space-y-1 text-base">
           <p>Under $50</p>
           <p>Under $100</p>
           <p>Premium</p>
@@ -25,11 +25,23 @@ const Sidebar = () => {
       {/* Color */}
       <div>
         <h3 className="font-semibold mb-2">Color</h3>
-        <div className="flex gap-2">
-          <span className="w-4 h-4 bg-black rounded-full"></span>
-          <span className="w-4 h-4 bg-blue-500 rounded-full"></span>
-          <span className="w-4 h-4 bg-orange-500 rounded-full"></span>
-          <span className="w-4 h-4 bg-gray-300 rounded-full"></span>
+        <div className="flex flex-col gap-4">
+          <label className="color-labels flex gap-2 justify-start items-center cursor-pointer">
+            <span className="w-4 h-4 bg-black rounded-full"></span>
+            <p>Black</p>
+          </label>
+          <label className="color-labels flex gap-2 justify-start items-center cursor-pointer">
+            <span className="w-4 h-4 bg-blue-500 rounded-full"></span>
+            <p>Blue</p>
+          </label>
+          <label className="color-labels flex gap-2 justify-start items-center cursor-pointer">
+            <span className="w-4 h-4 bg-orange-500 rounded-full"></span>
+            <p>Orange</p>
+          </label>
+          <label className="color-labels flex gap-2 justify-start items-center cursor-pointer">
+            <span className="w-4 h-4 bg-gray-300 rounded-full"></span>
+            <p>Grey</p>
+          </label>
         </div>
       </div>
     </div>
